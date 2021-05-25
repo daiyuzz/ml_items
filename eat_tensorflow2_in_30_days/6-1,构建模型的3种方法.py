7aebfa9258b808d5db2a46038e9bac946780d69e
daiyuzz <daiyu_zz@163.com>
2021-05-25 21:08:13 +0800

    feat：构建模型

---
+++ eat_tensorflow2_in_30_days/6-1,构建模型的3种方法.py
@@ -0,0 +1,116 @@
+import numpy as np
+import pandas as pd
+import tensorflow as tf
+from tqdm import tqdm
+from tensorflow.keras import *
+
+train_token_path = "./data/imdb/train_token.csv"
+test_token_path = "./data/imdb/test_token.csv"
+
+MAX_WORDS = 10000
+MAX_LEN = 200
+BATCH_SIZE = 20
+
+
+# 构建管道
+def parse_line(line):
+    t = tf.strings.split(line, "\t")
+    label = tf.reshape(tf.cast(tf.strings.to_number(t[0]), tf.int32), (-1,))
+    features = tf.cast(tf.strings.to_number(tf.strings.split(t[1], " ")), tf.int32)
+    return features, label
+
+
+ds_train = tf.data.TextLineDataset(filenames=[train_token_path]) \
+    .map(parse_line, num_parallel_calls=tf.data.experimental.AUTOTUNE) \
+    .shuffle(buffer_size=1000) \
+    .batch(BATCH_SIZE) \
+    .prefetch(tf.data.experimental.AUTOTUNE)
+# map：将转换函数映射到数据集的每一个元素,num_parallel_calls表示设置异步处理，提高性能，（AUTOTUNE）根据cpu动态调整
+# batch :构建批次
+# shuffle：数据顺序洗牌
+# prefetch：让数据准备和参数迭代两个过程相互并行
+
+ds_test = tf.data.TextLineDataset(filenames=[test_token_path]) \
+    .map(parse_line, num_parallel_calls=tf.data.experimental.AUTOTUNE) \
+    .shuffle(buffer_size=1000) \
+    .batch(BATCH_SIZE) \
+    .prefetch(tf.data.experimental.AUTOTUNE)
+
+# 一、Sequential按层顺序创建模型
+tf.keras.backend.clear_session()
+
+model = models.Sequential()
+
+model.add(layers.Embedding(MAX_WORDS,7,input_length=MAX_LEN))
+model.add(layers.Conv1D(filters = 64,kernel_size = 5,activation = "relu"))
+model.add(layers.MaxPool1D(2))
+model.add(layers.Conv1D(filters = 32,kernel_size = 3,activation = "relu"))
+model.add(layers.MaxPool1D(2))
+model.add(layers.Flatten())
+model.add(layers.Dense(1,activation = "sigmoid"))
+
+model.compile(optimizer='Nadam',
+            loss='binary_crossentropy',
+            metrics=['accuracy',"AUC"])
+
+model.summary()
+
+import datetime
+baselogger = callbacks.BaseLogger(stateful_metrics=["AUC"])
+logdir = "./data/keras_model/" + datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
+tensorboard_callback = tf.keras.callbacks.TensorBoard(logdir, histogram_freq=1)
+history = model.fit(ds_train,validation_data = ds_test,
+        epochs = 6,callbacks=[baselogger,tensorboard_callback])
+
+import matplotlib.pyplot as plt
+
+
+def plot_metric(history, metric):
+    train_metrics = history.history[metric]
+    val_metrics = history.history['val_' + metric]
+    epochs = range(1, len(train_metrics) + 1)
+    plt.plot(epochs, train_metrics, 'bo--')
+    plt.plot(epochs, val_metrics, 'ro-')
+    plt.title("Training and Validation" + metric)
+    plt.xlabel("Epochs")
+    plt.ylabel(metric)
+    plt.legend(["train_" + metric, 'val_' + metric])
+    plt.show()
+
+
+plot_metric(history, "AUC")
+
+
+
+# 二、函数式API创建任意结构模型
+
+tf.keras.backend.clear_session()
+
+inputs = layers.Input(shape=[MAX_LEN])
+x  = layers.Embedding(MAX_WORDS,7)(inputs)
+
+branch1 = layers.SeparableConv1D(64,3,activation="relu")(x)
+branch1 = layers.MaxPool1D(3)(branch1)
+branch1 = layers.SeparableConv1D(32,3,activation="relu")(branch1)
+branch1 = layers.GlobalMaxPool1D()(branch1)
+
+branch2 = layers.SeparableConv1D(64,5,activation="relu")(x)
+branch2 = layers.MaxPool1D(5)(branch2)
+branch2 = layers.SeparableConv1D(32,5,activation="relu")(branch2)
+branch2 = layers.GlobalMaxPool1D()(branch2)
+
+branch3 = layers.SeparableConv1D(64,7,activation="relu")(x)
+branch3 = layers.MaxPool1D(7)(branch3)
+branch3 = layers.SeparableConv1D(32,7,activation="relu")(branch3)
+branch3 = layers.GlobalMaxPool1D()(branch3)
+
+concat = layers.Concatenate()([branch1,branch2,branch3])
+outputs = layers.Dense(1,activation = "sigmoid")(concat)
+
+model = models.Model(inputs = inputs,outputs = outputs)
+
+model.compile(optimizer='Nadam',
+            loss='binary_crossentropy',
+            metrics=['accuracy',"AUC"])
+
+model.summary()
---
+++ eat_tensorflow2_in_30_days/6-2,训练模型的3种方法.py
@@ -0,0 +1,22 @@
+import numpy as np
+import pandas as pd
+import tensorflow as tf
+from tensorflow.keras import *
+
+
+# 打印时间分割线
+@tf.function
+def printbar():
+    today_ts = tf.timestamp() % (24 * 60 * 60)
+    hours = tf.cast(today_ts // 3600 + 8, tf.int32) % tf.constant(24)
+    minite = tf.cast((today_ts % 3600) // 60, tf.int32)
+    second = tf.cast(tf.floor(today_ts % 60), tf.int32)
+
+    def timeformat(m):
+        if tf.strings.length(tf.strings.format("{}", m)) == 1:
+            return (tf.strings.format("0{}", m))
+        else:
+            return (tf.strings.format("{}", m))
+
+    timestring = tf.strings.join([timeformat(hours),timeformat(minite),timeformat(second)],separator=":")
+    tf.print("====="*8 + timestring)
